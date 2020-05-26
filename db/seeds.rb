6fe47ae71d4619794a1861ce2c1fce78c3ea6145
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-05-26 16:59:05 +0100

    created the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "delete the users"
+User.destroy_all
+puts "create user"
+user = User.create!(email: "user@user.com",password:"123456", first_name: "user", last_name:"last_name")
+puts "user created"
+
+puts "delete the group"
+Group.destroy_all
+puts "create group"
+group = Group.create!(name: "Rails masters", description: "group about Ruby on Rails")
+puts "group created"
+
+puts "delete the membership"
+Membership.destroy_all
+puts "create membership"
+membership = Membership.create!(user: user, group: group)
+puts "membership created"
+
+puts "delete the posts"
+Post.destroy_all
+puts "create post"
+post = Post.create!(content: "This is the first post", user: user, group: group)
+puts "post created"
+
+puts "delete the comment"
+Comment.destroy_all
+puts "create comment"
+comment = Comment.create!(content: "This is the first comment", post: post, user: user)
+puts "comment created"
+
+puts "delete the bookmark"
+Bookmark.destroy_all
+puts "create bookmark"
+bookmark = Bookmark.create!(post: post, user: user)
+puts "bookmark created"
+
+
+
+
