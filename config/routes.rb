e34fb490815f3551b7e1c92d321ce56efaa40601
Arthur Littmann <arthur@lewagon.org>
2020-05-26 15:40:45 +0100

    added navbar and fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :groups, only: [:index, :show] do
     resources :membership, only: [:create]
-    resources :posts, only: [:create] do
-      resources :comments, only: [:create]
-      resources :bookmarks, only: [:create]
-    end 
+    resources :posts, only: [:create]
   end
-  resources :posts, only: [:destroy]
+
+  resources :posts, only: [:destroy] do
+    resources :comments, only: [:create]
+    resources :bookmarks, only: [:create]
+  end
+
   resources :comments, only: [:destroy]
 end
