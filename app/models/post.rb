414f740c3f6ebda05c403bc5f59aa300a4f116d9
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-05-29 15:04:26 +0100

    Links

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,19 @@
+
+require "uri"
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
   has_many :bookmarks, dependent: :destroy
   validates :content, presence: true
   has_many :comments, dependent: :destroy
+
+  def content_with_links
+
+    urls = URI.extract(content, ['http', 'https'])
+    urls.each do |url|
+      url.delete_suffix!(url[-1]) if url[-1].match?(/\W/)
+      content.gsub!(url, "<a href='#{url}' target='_blank' class='primary-text'>#{url}</a>")
+    end
+    content.html_safe
+  end 
 end
