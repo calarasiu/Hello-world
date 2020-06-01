f6d5f28b9fe7cfadf5b3cfb323c91c2bb4c0a677
sandy smircic <sandy.smircic@hotmail.com>
2020-06-01 17:50:34 +0100

    First edit

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,7 @@
 class NotificationsController < ApplicationController
+  def index
+    @notifications = Notification.where(user: current_user, read: false)
+    @notifications.update(read: true)
+    @unread_notifications = 0
+  end
 end
