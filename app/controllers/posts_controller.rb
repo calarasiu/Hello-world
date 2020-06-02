3c7d9cff774185b0a6bde397e9dc4797547ec8b9
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-06-02 18:44:52 +0100

    Heroku error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
   def index
-    # @group = Group.find(params[:group_id])
     @posts = current_user.posts
   end
 
@@ -14,14 +13,12 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @group = Group.find(params[:group_id])
     @post.group = @group
-    if @post.user = current_user #for now we are just checking if the user is loged in
+    if @post.user = current_user 
       @post.save
       redirect_to group_path(@group, anchor: "post-#{@post.id}")
     else
       flash[:notice] = "You need to login before adding a post"
-      # render "posts/new" we don't need to render because we are staying on the same page
-
-    end
+      end
   end
 
   def destroy
