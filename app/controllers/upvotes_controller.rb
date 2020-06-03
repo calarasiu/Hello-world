f20a83c9aa874351c4f79c088ffc5ba3deeb667f
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-06-03 16:37:59 +0100

    twiter

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -4,7 +4,8 @@ class UpvotesController < ApplicationController
   def create 
     @upvote = Upvote.create(user_id: current_user.id, post_id: @post.id)
     @upvote.save 
-    redirect_to group_path(@post.group.id, anchor: "upvote-#{@post.id}")
+    redirect_to group_path(@post.group.id, anchor: "post-#{@post.id}")
+    # redirect_back(fallback_location: group_path(@post.group.id)
   end 
 
   private
